9146ecda1d09acc66770877e815d1869c5599358
César M. Urdaneta <upcesar@gmail.com>
2017-08-28 04:23:56 -0300

    Added: AXIOS HTTP Client, Font-Awesome, Vue JS. Returning token when authenticated.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,6 +17,78 @@ window.Vue = require('vue');
 
 Vue.component('example', require('./components/Example.vue'));
 
-const app = new Vue({
-    el: '#app'
+/**
+ * Passport component
+ */
+
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue')
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue')
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue')
+);
+
+
+new Vue({
+    el: '#formLogin',
+    data: {
+        user: {
+            'grant_type': 'password',
+            'username': null,
+            'password': null,
+            'client_id': 1,
+            'client_secret':'SL6U6Mg7ZBj9EpcdFf0DgkAzGnyLYvQmA7a3RCCG'
+        },
+        submitData: null,
+        loading : false,
+        submitted : false, 
+        seen: true,
+    },
+    methods: {
+        doLogin: function() {
+            
+            const outerResp = this;
+            outerResp.loading = true;
+            outerResp.submitted = true;
+            outerResp.submitData = null;
+            
+            axios.post('http://www.logcomexlogin.local/oauth/token', this.user)
+                .then(response => {
+                  // JSON responses are automatically parsed.
+                  outerResp.submitData = 'Auth OK | ' + response.data.token_type + ' ' + response.data.access_token;
+                  outerResp.loading = false;
+                })
+                .catch(e => {
+                    if (e.response) {
+                        // The request was made and the server responded with a status code
+                        // that falls out of the range of 2xx
+                        outerResp.submitData = e.response.data;
+                        console.log(e.response.data);
+                        console.log(e.response.status);
+                        console.log(e.response.headers);
+                    } else if (e.request) {
+                        // The request was made but no response was received
+                        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                        // http.ClientRequest in node.js
+                        outerResp.submitData = e.message;
+                        console.log(e.request);
+                    } else {
+                        // Something happened in setting up the request that triggered an Error
+                        outerResp.submitData = e.message;
+                        console.log('Error', e.message);
+                    }
+                    outerResp.loading = false;
+                });
+            
+            
+        }
+    }
 });
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,14 +8,14 @@
                 <div class="panel-heading">Login</div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                    <form class="form-horizontal" v-on:submit.prevent="doLogin" id="formLogin">
                         {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        
+                        <div class="form-group">
+                            <label for="email"  class="col-md-4 control-label">E-Mail</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" class="form-control" name="email" v-model="user.username" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -25,11 +25,11 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                        <div class="form-group">
+                            <label for="password" class="col-md-4 control-label">Senha</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control" name="password" v-model="user.password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -38,29 +38,27 @@
                                 @endif
                             </div>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
+                        
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
                                     Login
                                 </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
+                                <span v-if="loading">
+                                    <i class="fa fa-spinner fa-pulse fa-fw"></i>
+                                    <span>Autenticando...</span>
+                                </span>
                             </div>
                         </div>
+                        
+                        <div class="form-group" v-if="submitData">
+                            <div class="col-md-8 col-md-offset-4">
+                                @{{ submitData }}
+                            </div>
+                        </div>
+                        
                     </form>
+
                 </div>
             </div>
         </div>
